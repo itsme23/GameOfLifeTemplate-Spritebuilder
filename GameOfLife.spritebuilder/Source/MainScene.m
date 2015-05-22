8fab3157f408f5b0097ef3c97e50ae15314bc5f5
M. B. Meyer <mbmeyer@Ms-iMac.local>
2015-05-22 12:17:16 +0200

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Source/MainScene.m
+++ GameOfLife.spritebuilder/Source/MainScene.m
@@ -7,7 +7,91 @@
 //
 
 #import "MainScene.h"
+#import "Grid.h"
 
-@implementation MainScene
+@implementation MainScene {
+    Grid *_grid;
+    CCTimer *_timer;
+    CCLabelTTF *_generationLabel;
+    CCLabelTTF *_populationLabel;
+}
+
+- (id)init
+{
+    self = [super init];
+    
+    if (self) {
+        _timer = [[CCTimer alloc] init];
+    }
+    
+    return self;
+}
+
+- (void)play
+{
+    //this tells the game to call a method called 'step' every half second.
+    [self schedule:@selector(step) interval:0.5f];
+}
+
+- (void)pause
+{
+    [self unschedule:@selector(step)];
+}
+
+// this method will get called every half second when you hit the play button and will stop getting called when you hi the pause button
+- (void)step
+{
+    [_grid evolveStep];
+    _generationLabel.string = [NSString stringWithFormat:@"%d", _grid.generation];
+    _populationLabel.string = [NSString stringWithFormat:@"%d", _grid.totalAlive];
+    
+    //update each Creature's neighbor count
+    [self countNeighbors];
+    
+    //update each Creature's state
+    [self updateCreatures];
+    
+    //update the generation so the label's text will display the correct generation
+    _generation++;
+}
+
+// iterate through the rows
+// note that NSArray has a method 'count' that will return the number of elements in the array
+for (int i = 0; i < [_gridArray count]; i++)
+{
+    // iterate through all the columns for a given row
+    for (int j = 0; j < [_gridArray[i] count]; j++)
+    {
+        // access the creature in the cell that corresponds to the current row/column
+        Creature *currentCreature = _gridArray[i][j];
+        
+        // remember that every creature has a 'livingNeighbors' property that we created earlier
+        currentCreature.livingNeighbors = 0;
+        
+        // now examine every cell around the current one
+        
+        // go through the row on top of the current cell, the row the cell is in, and the row past the current cell
+        for (int x = (i-1); x <= (i+1); x++)
+        {
+            // go through the column to the left of the current cell, the column the cell is in, and the column to the right of the current cell
+            for (int y = (j-1); y <= (j+1); y++)
+            {
+                // check that the cell we're checking isn't off the screen
+                BOOL isIndexValid;
+                isIndexValid = [self isIndexValidForX:x andY:y];
+                
+                // skip over all cells that are off screen AND the cell that contains the creature we are currently updating
+                if (!((x == i) && (y == j)) && isIndexValid)
+                {
+                    Creature *neighbor = _gridArray[x][y];
+                    if (neighbor.isAlive)
+                    {
+                        currentCreature.livingNeighbors += 1;
+                    }
+                }
+            }
+        }
+    }
+}
 
 @end
